4fa20aed19e214701ae8b39209c55007c5340a1e
XimeCruz <xmenacruzt@gmail.com>
2022-01-25 19:19:02 -0400

    Initial commit

---
+++ src/test/java/bo/entereza/celulares/CelularesApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.entereza.celulares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelularesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
